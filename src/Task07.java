c43d5d6b2ba7b56b6269d51422809f0d56f77acc
PolinaKv <polina110704kovaloyva@gmail.com>
2022-09-13 12:01:42 +0300

    Initial commit

---
+++ src/Task07.java
@@ -0,0 +1,20 @@
+public class Task07 {
+
+    public static int sum(int a, int b) {
+        int result = a + b;
+        return result;
+
+    }
+
+    public static void print(int result) {
+
+        System.out.println(result);
+        return;
+    }
+
+    public static void main(String[] args) {
+
+
+
+    }
+}
